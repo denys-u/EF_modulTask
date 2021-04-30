dd594d123e792d018f04866395ccc2296b125470
denys-u <denys.udovik@nure.ua>
2021-04-30 11:10:58 +0300

    created entyties

---
+++ EF_modulTask/EF_modulTask/Entyties/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_modulTask.Entyties
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ EF_modulTask/EF_modulTask/Entyties/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_modulTask.Entyties
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Duration { get; set; }
+
+        public DateTime ReleasedDate { get; set; }
+    }
+}
