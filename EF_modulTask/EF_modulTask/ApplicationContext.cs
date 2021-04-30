5930e331456f2819e05ae7f6ebeab161f4527bac
denys-u <denys.udovik@nure.ua>
2021-04-30 11:31:28 +0300

    added json file and ApplicationContext

---
+++ EF_modulTask/EF_modulTask/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using EF_modulTask.Entyties;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EF_modulTask
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext([NotNullAttribute] DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Song> Songs { get; set; }
+    }
+}
--- EF_modulTask/EF_modulTask/Program.cs
+++ EF_modulTask/EF_modulTask/Program.cs
@@ -2,11 +2,11 @@
 
 namespace EF_modulTask
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
